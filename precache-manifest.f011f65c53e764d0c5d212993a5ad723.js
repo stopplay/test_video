342219cc172bc2ef1c67b49df1000de0ab4ea794
Jimmy <jimmy.arbid@gmail.com>
2020-03-10 16:00:11 -0300

    Updates

---
+++ precache-manifest.f011f65c53e764d0c5d212993a5ad723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34b26b0e3a490d26c38fecd15668423",
+    "url": "/test_video/index.html"
+  },
+  {
+    "revision": "c7c35b5e55c7fd47ce5a",
+    "url": "/test_video/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f9b5f8bce62c23f14a23",
+    "url": "/test_video/static/js/2.677eab81.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test_video/static/js/2.677eab81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c35b5e55c7fd47ce5a",
+    "url": "/test_video/static/js/main.3966d6fb.chunk.js"
+  },
+  {
+    "revision": "6097fa35dd4c35625b04",
+    "url": "/test_video/static/js/runtime-main.0220e2bd.js"
+  }
+]);
